3c93e448f088e5839f3cb19ee0f94ee3272e5b21
Pro-Panda <f2016015@pilani.bits-pilani.ac.in>
2017-10-17 15:25:17 +0530

    Renaming variables, and removing redundant ones

--- Day-Finder.c
+++ Day-Finder.c
@@ -1,282 +1,287 @@
 //Warning! This is a horror. I knew little programming practices while writing this.
 #include<stdio.h>
 #include<time.h>
+
+int MONTH_DATES[12] = {31,29,31,30,31,30,31,31,30,31,30,31};
+int MONTH_CODES[12] = {6,2,2,5,0,3,5,1,4,6,2,4};
+int MONTH_CODES_LEAP[12] = {5,1,2,5,0,3,5,1,4,6,2,4};
+int YEAR_CODES[4] = {0,5,3,1};
+char INVALID_MSG[35] = "Invalid Date. Please try again.";
+char DAYS[7][10] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+
 int main ()
 {
-     int a, b, c, d, e, f, g, h, i, j, k, l, m, n, p, q, r, s, t, u, v, w, x;
-     char o;
-     n=2;
-     printf("\n                  Welcome to Calender Day Finder. \nType any date and get the day\n");
-     printf("                                              Developed by Rahul Bothra\n                                  ");
+     int date,year, month;
+     int year_code, month_code;
+     int year_last_2;
+     int sum_codes, answer;
+     int loop_counter;
+     int part_a, part_b;
+     printf("\n\t\t\tWelcome to Calender Day Finder. \nType any date and get the day\n");
+     printf("\t\t\t\t\t\tDeveloped by Rahul Bothra\n");
      lp:
      printf("\nPlease type the Date (dd):\n");
-     scanf("%d" ,& a);
-     if((a<=0) || (a>=32))
-     {
-               printf("Invalid date. Please try again");
-               goto lp;
-     }
-     e=a%7;
+     scanf("%d" ,&date);
      printf("Please type the year: (yyyy)\n");
-     scanf("%d", & b);
-       for(q=0;q<400;q++)
+     scanf("%d", &year);
+       for(loop_counter=0;loop_counter<400;loop_counter++)
     {
-                      r=b-q;
-                      if(r%400==0)
+            int temp;
+                      temp=year-loop_counter;
+                      if(temp%400==0)
                       {
-                                  s=q;
-                                  if((s>=0) && (s<=99))
-                                  t=0;
-                                  else if((s>=100) && (s<=199))
-                                  t=5;
-                                  else if((s>=200) && (s<=299))
-                                  t=3;
-                                  else if((s>=300) && (s<=399))
+                                  if((loop_counter>=0) && (loop_counter<=99))
+                                  year_code=0;
+                                  else if((loop_counter>=100) && (loop_counter<=199))
+                                  year_code=5;
+                                  else if((loop_counter>=200) && (loop_counter<=299))
+                                  year_code=3;
+                                  else if((loop_counter>=300) && (loop_counter<=399))
                                   {
-                                      t=1;
+                                      year_code=1;
                                   }
                       }
                       
     }
-    for(u=0;u<100;u++)
+    for(loop_counter=0;loop_counter<100;loop_counter++)
     {
-                      v=b-u;
-                      if(v%100==0)
+                      int temp=year-loop_counter;
+                      if(temp%100==0)
                       {
-                                  w=v;
-                                  x=b-v;
-                                  d=x%7;
-                                  f=x/4;                                  
+                                  year_last_2=year-temp;
+                                  part_a=year_last_2%7;
+                                  part_b=year_last_2/4;                                  
                       }
                       }
                           
           
-     printf("Please type the month no. that is against the month name \n 1  | January\n 2  | February\n 3  | March\n ");
-     printf("4  | April\n 5  | May\n 6  | June\n 7  | July\n 8  | August\n 9  | September\n 10 | October\n 11 | November \n 12 | December\n "); 
+     printf("Please type the month no. that is against the month name \n 1  | January\n 2  | February\n 3  | March\n \
+4  | April\n 5  | May\n 6  | June\n 7  | July\n 8  | August\n 9  | September\n 10 | October\n 11 | November \n 12 | December\n "); 
      gp:
-                scanf("%d" ,& g);
+                scanf("%d" ,&month);
 
-     switch (g)
+     switch (month)
      {
             case 1:
-                 if(a>31)
-                 {
-                         printf("Invalid date. Please try again");
-                         goto lp;
-                 }
-                 else
-                 {
-                     if ((b%100==0) && (b%400!=0))
-                 {
-                      h=6;
-                 }
-                 else if ((b%100==0) && (b%400==0))
-                 {
-                      h=5;
-                 }
-                 else if(b%4==0)
-                 {
-                      h=5;
-                 }
-                 else
-                 h=6;
-                 }
-                 break;
-                 case 2:
-                      if((b%4==0) && (a>29))
-                      {
-                              printf("Invalid date. Please try again.");
-                              goto lp;
-                      }
-                      else if ((b%4==0) && (a<=29))  
-                      {
-                           if(b%100!=0)
-                           {
-                           h=1;
-                           }
-                           else if((b%100==0) && (b%400!=0))
-                           {
-                                if(a>28)
+                if(date>31)
+                {
+                        printf("%s",INVALID_MSG);
+                        goto lp;
+                }
+                else
+                {
+                        if ((year%100==0) && (year%400!=0))
+                        {
+                                month_code=6;
+                        }
+                        else if ((year%100==0) && (year%400==0))
+                        {
+                                month_code=5;
+                        }
+                        else if(year%4==0)
+                        {
+                                month_code=5;
+                        }
+                        else
+                                month_code=6;
+                }
+                break;
+                case 2:
+                if((year%4==0) && (date>29))
+                {
+                        printf("%s",INVALID_MSG);
+                        goto lp;
+                }
+                else if ((year%4==0) && (date<=29))  
+                {
+                        if(year%100!=0)
+                        {
+                        month_code=1;
+                        }
+                        else if((year%100==0) && (year%400!=0))
+                        {
+                                if(date>28)
                                 {
-                                    printf("Invalid date. Please try again.");
-                              goto lp;
-                              }
-                              else
-                              h=2;
-                           }
-                      else if((b%100==0) && (b%400==0))
-                      {
-                           if(a<=29)
-                           {
-                                   h=1;
-                                   }
-                           else
-                           {
-                               printf("Invalid date. Please try again.");
-                              goto lp;
-                           }
-                      }
-                      }
-                      else if (a>28)
-                      {
-                              printf("Invalid date. Please try again.");
-                              goto lp;
-                      }
-                      else
-                         {
-                              abd:
-                     if ((b%100==0) && (b%400!=0))
-                 {
-                      h=2;
-                 }
-                 else if ((b%100==0) && (b%400==0))
-                 {
-                      h=1;
-                 }
-                 else if(b%4==0)
-                 {
-                      h=1;
-                 }
-                 else
-                 h=2;
-                 }
-                      break;
-                      case 3:
-                            if(a>31)
-                      {
-                              printf("Invalid date. Please try again.");
-                              goto lp;
-                      }
-                       else
-                       h=2;
-                       break;
-                           case 4:
-                                if(a>30)
-                      {
-                              printf("Invalid date. Please try again.");
-                              goto lp;
-                      }
-                      else
-                      
-                                h=5;
+                                printf("%s",INVALID_MSG);
+                        goto lp;
+                        }
+                        else
+                        month_code=2;
+                        }
+                else if((year%100==0) && (year%400==0))
+                {
+                        if(date<=29)
+                        {
+                                month_code=1;
+                                }
+                        else
+                        {
+                        printf("%s",INVALID_MSG);
+                        goto lp;
+                        }
+                }
+                }
+                else if (date>28)
+                {
+                        printf("%s",INVALID_MSG);
+                        goto lp;
+                }
+                else
+                        {
+                        abd:
+                if ((year%100==0) && (year%400!=0))
+                {
+                month_code=2;
+                }
+                else if ((year%100==0) && (year%400==0))
+                {
+                month_code=1;
+                }
+                else if(year%4==0)
+                {
+                month_code=1;
+                }
+                else
+                month_code=2;
+                }
+                break;
+                case 3:
+                        if(date>31)
+                {
+                        printf("%s",INVALID_MSG);
+                        goto lp;
+                }
+                else
+                month_code=2;
+                break;
+                        case 4:
+                                if(date>30)
+                {
+                        printf("%s",INVALID_MSG);
+                        goto lp;
+                }
+                else
+                
+                                month_code=5;
                                 break;
                                 case 5:
-                                     if(a>31)
-                      {
-                              printf("Invalid date. Please try again.");
-                              goto lp;
-                      }
-                      else
-                                     h=0;
-                                     break;
-                                     case 6:
-                                          if(a>30)
-                      {
-                              printf("Invalid date. Please try again.");
-                              goto lp;
-                      }
-                      else
-                                          h=3;
-                                          break;
-                                          case 7:
-                                               if(a>31)
-                      {
-                              printf("Invalid date. Please try again.");
-                              goto lp;
-                      }
-                      else
-                                               h=5;
-                                               break;
-                                               case 8:
-                                                    if(a>31)
-                      {
-                              printf("Invalid date. Please try again.");
-                              goto lp;
-                      }
-                      else
-                                                    h=1;
-                                                    break;
-                                                    case 9:
-                                                         if(a>30)
-                      {
-                              printf("Invalid date. Please try again.");
-                              goto lp;
-                      }
-                      else
-                                                         h=4;
-                                                         break;
-                                                         case 10:
-                                                              if(a>31)
-                      {
-                              printf("Invalid date. Please try again.");
-                              goto lp;
-                      }
-                      else
-                                                              h=6;
-                                                              break;
-                                                              case 11:
-                                                                   if(a>30)
-                      {
-                              printf("Invalid date. Please try again.");
-                              goto lp;
-                      }
-                      else
-                                                                   h=2;
-                                                                   break;
-                                                                   case 12:
-                                                                        if(a>31)
-                      {
-                              printf("Invalid date. Please try again.");
-                              goto lp;
-                      }
-                      else
-                      h=4;
-                      break;
-                      default:
-                      printf("Invalid Choice. Please type again.");
-                      goto gp;
+                                if(date>31)
+                {
+                        printf("%s",INVALID_MSG);
+                        goto lp;
+                }
+                else
+                                month_code=0;
+                                break;
+                                case 6:
+                                        if(date>30)
+                {
+                        printf("%s",INVALID_MSG);
+                        goto lp;
+                }
+                else
+                                        month_code=3;
+                                        break;
+                                        case 7:
+                                        if(date>31)
+                {
+                        printf("%s",INVALID_MSG);
+                        goto lp;
+                }
+                else
+                                        month_code=5;
+                                        break;
+                                        case 8:
+                                                if(date>31)
+                {
+                        printf("%s",INVALID_MSG);
+                        goto lp;
+                }
+                else
+                                                month_code=1;
+                                                break;
+                                                case 9:
+                                                        if(date>30)
+                {
+                        printf("%s",INVALID_MSG);
+                        goto lp;
+                }
+                else
+                                                        month_code=4;
+                                                        break;
+                                                        case 10:
+                                                        if(date>31)
+                {
+                        printf("%s",INVALID_MSG);
+                        goto lp;
+                }
+                else
+                                                        month_code=6;
+                                                        break;
+                                                        case 11:
+                                                                if(date>30)
+                {
+                        printf("%s",INVALID_MSG);
+                        goto lp;
+                }
+                else
+                                                                month_code=2;
+                                                                break;
+                                                                case 12:
+                                                                        if(date>31)
+                {
+                        printf("%s",INVALID_MSG);
+                        goto lp;
+                }
+                else
+                month_code=4;
+                break;
+                default:
+                printf("Invalid Choice. Please type again.");
+                goto gp;
      }
-     i=d+f+h+t+a;
+     sum_codes=part_a+part_b+month_code+year_code+date;
      
-     j=i%7;
-     if(j==0)
+     answer=sum_codes%7;
+     if(answer==0)
      printf("\nIt is Sunday.\n");
-     else if (j==1)
+     else if (answer==1)
      printf("\nIt is Monday.\n");
-     else if(j==2)
+     else if(answer==2)
      printf("\nIt is Tuesday.\n");
-     else if(j==3)
+     else if(answer==3)
      printf("\nIt is Wednesday.\n");
-     else if(j==4)
+     else if(answer==4)
      printf("\nIt is Thursday.\n");
-     else if(j==5)
+     else if(answer==5)
      printf("\nIt is Friday.\n");
      else
      {
          printf("\nIt is Saturday.\n");
      }
      
-      if((a==2) && (g==10))
+      if((date==2) && (month==10))
                printf("This is Gandhi Jayanti.");
-     else if((a==15) && (g==8))
+     else if((date==15) && (month==8))
           printf("This is India's Independence Day.\n");
-     else if((a==25) && (g==12))
+     else if((date==25) && (month==12))
           printf("This is Christmas.\n");
-     else if ((a==14) && (g==1))
+     else if ((date==14) && (month==1))
           printf("This is Makar Sakranti.\n");
-     else if((a==26) && (g==1))
+     else if((date==26) && (month==1))
           printf("This is India's Republic Day\n");
-     else if((a==1) && (g==5))
+     else if((date==1) && (month==5))
           printf("This is May Day.\n");
-     else if((a==5) && (g==6))
+     else if((date==5) && (month==6))
           printf("This is Rahul Bothra's Birthday.\n");
      else
      {
      }
      
      ad:
+     int m;
           printf("\nPress 1 to find another day.\nPress 2 to exit\n");
-     scanf("%d" , & m);
+     scanf("%d" , &m);
      if (m==1)
      {
      goto lp;
@@ -285,7 +290,8 @@ int main ()
      {
          printf("Are you sure you want to Exit?\nPress y to confirm or n to go back.\n");
          mp:
-                     scanf("%s" ,& o);
+         char o;
+                     scanf("%s" ,&o);
          switch (o)
          {
                 case 'y':
@@ -305,4 +311,4 @@ int main ()
          printf("Wrong Choice. Please Try Again");
          goto ad;
      }
-     }  
+}
