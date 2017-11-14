6377d5efcb9498720ed8093820718a7151480e31
Pro-Panda <f2016015@pilani.bits-pilani.ac.in>
2017-11-14 21:51:09 +0530

    Update: Removed separate codes for leap years. Moved test function to Testing Module

--- Get-Day.c
+++ Get-Day.c
@@ -1,65 +1,40 @@
 #include <stdio.h>
 
-int MONTH_DATES[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-int MONTH_DATES_LEAP[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-int MONTH_CODES[12] = {6, 2, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
-int MONTH_CODES_LEAP[12] = {5, 1, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
-int YEAR_CODES[4] = {0, 5, 3, 1};
-
-int date, year, month, leap;
-int year_code, month_code, part_a, part_b;
-int year_last_2, year_first_2;
-int answer;
+const int MONTH_DATES[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+const int MONTH_CODES[12] = {6, 2, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
+const int YEAR_CODES[4] = {0, 5, 3, 1};
 
 int leap_check(int year){
-    if(year%4!=0)
-            return 0;
-    else if(year%100!=0 || year%400==0)
-            return 1;
+    if(year%4==0 && (year%100!=0 || year%400==0))
+        return 1;
     return 0;
 }
 
-int test_print()
-{
-        printf("date: %d\n",date);
-        printf("year: %d\n",year);
-        printf("month: %d\n",month);
-        printf("year_code: %d\n",year_code);
-        printf("month_code: %d\n",month_code);
-        printf("part_a: %d\n",part_a);
-        printf("part_b: %d\n",part_b);
-        printf("leap: %d\n",leap);
-        printf("year_first_2: %d\n",year_first_2);
-        printf("year_last_2: %d\n",year_last_2);
-        printf("answer: %d\n",answer);
-}
-
 int get_day(int date, int month, int year){
+    int year_code, month_code, part_a, part_b;
+    int year_last_2, year_first_2;
+    int leap, answer;
+
     year_first_2 = year/100;
     year_code = YEAR_CODES[year_first_2%4];
     year_last_2 = year%100;
     part_a = year_last_2%7;
     part_b = year_last_2/4;
     leap = leap_check(year);
-    if(month>12 || month<0 || date<0)
-    {
-            return 404;
+    if(month>12 || month<0 || date<0){
+        return 404;
     }
-    if (leap)
-    {
-            month_code = MONTH_CODES_LEAP[month-1];
-            if (date>MONTH_DATES_LEAP[month-1])
-            {
-                    return 404;
-            }
+    if (leap && month<3){
+        month_code = MONTH_CODES[month-1]-1;
+        if ((date>29 && month==2) || (date>31 && month==1)){
+                return 404;
+        }
     }
-    else
-    {
-            month_code = MONTH_CODES[month-1];
-            if (date>MONTH_DATES[month-1])
-            {
-                    return 404;
-            }
+    else{
+        month_code = MONTH_CODES[month-1];
+        if (date>MONTH_DATES[month-1]){
+                return 404;
+        }
     }
     answer = (part_a + part_b + month_code + year_code + date) % 7;
     // test();    
--- Testing-Module.c
+++ Testing-Module.c
@@ -4,6 +4,22 @@
 
 char DAYS[7][10] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
+//Use and call this test function in 'Get-Day' for printing all values
+int test_print()
+{
+        printf("date: %d\n",date);
+        printf("year: %d\n",year);
+        printf("month: %d\n",month);
+        printf("year_code: %d\n",year_code);
+        printf("month_code: %d\n",month_code);
+        printf("part_a: %d\n",part_a);
+        printf("part_b: %d\n",part_b);
+        printf("leap: %d\n",leap);
+        printf("year_first_2: %d\n",year_first_2);
+        printf("year_last_2: %d\n",year_last_2);
+        printf("answer: %d\n",answer);
+}
+
 int main()
 {
     int date, month, year, value;
